d0aa41bca190157b69b4f7b7fc860e62e63bab65
Kelly <31076298+kbomb6@users.noreply.github.com>
2025-04-08 21:01:59 -0600

    Add investor pages

--- src/views/Demo.vue
+++ src/views/Demo.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-      Demo Page
+        <div style="position: relative; box-sizing: content-box; max-height: 80vh; max-height: 80svh; width: 100%; aspect-ratio: 0.48488120950323976; padding: 40px 0 40px 0;"><iframe src="https://app.supademo.com/embed/cm91i8ojs206feyp0mwz6zj4d?embed_v=2" loading="lazy" title="Lucky Friday Demo" allow="clipboard-write" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe></div>
     </div>
   </template>
   
